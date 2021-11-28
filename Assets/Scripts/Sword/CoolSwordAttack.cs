373dff4d31c103e034ce44998a46870d7aeadc5b
Strafeken <strafeken@gmail.com>
2021-11-28 22:48:44 +0800

    Freestyle swing

--- Assets/Scripts/Sword/CoolSwordAttack.cs
+++ Assets/Scripts/Sword/CoolSwordAttack.cs
@@ -18,7 +18,6 @@ public class CoolSwordAttack : MonoBehaviour
     private Quaternion firstRotation;
 
     [SerializeField] private float swingSpeed = 1f;
-    float minDistance = 0.01f;
 
     public Transform[] controlPoints;
     private Vector3 gizmosPosition;
@@ -31,6 +30,11 @@ public class CoolSwordAttack : MonoBehaviour
 
     [SerializeField] private Transform raycastPoint;
     [SerializeField] private Transform raycastPointDirection;
+
+
+    private int previousSide = -1;
+
+    [SerializeField] private MouseControl mouseControl;
 
     enum Side
     {
@@ -78,9 +82,9 @@ public class CoolSwordAttack : MonoBehaviour
 
                 controlPoints[3].position = endPosition;
 
-                Vector3 resultantVector = (controlPoints[3].position - swordTip.position).normalized;
-                controlPoints[1].position = controlPoints[0].position + (0.3f * resultantVector);
-                controlPoints[2].position = controlPoints[0].position + (0.6f * resultantVector);
+                Vector3 midPoint = (controlPoints[0].position + controlPoints[3].position) * 0.5f;
+                controlPoints[1].position = midPoint;
+                controlPoints[2].position = midPoint;
 
                 StartCoroutine("Swing");
             }
@@ -102,6 +106,7 @@ public class CoolSwordAttack : MonoBehaviour
 
     private IEnumerator Swing()
     {
+        mouseControl.enabled = false;
         Vector3 p0 = controlPoints[0].position;
         Vector3 p1 = controlPoints[1].position;
         Vector3 p2 = controlPoints[2].position;
@@ -120,6 +125,10 @@ public class CoolSwordAttack : MonoBehaviour
             transform.position = nextPosition;
             yield return null;
         }
+        mouseControl.enabled = true;
+
+        //transform.localPosition = initialPosition;
+        //transform.localRotation = initialRotation;
     }
 
     private bool PlaceTarget()
@@ -141,34 +150,77 @@ public class CoolSwordAttack : MonoBehaviour
         return false;
     }
 
+    private Vector3 FindPointOfIntersectionWithScreenBorder(Vector3 origin, Vector3 direction)
+    {
+        Ray ray = new Ray(origin, direction);
+        Plane[] planes = GeometryUtility.CalculateFrustumPlanes(playerCamera);
+        Vector3 hitPoint = Vector3.zero;
+
+        // Raycast against the plane
+        if (planes[FindClosestPlane()].Raycast(ray, out float distance))
+        {
+            hitPoint = ray.GetPoint(distance);
+        }
+
+        hitPoint -= swingDirection * 0.2f;
+
+        return hitPoint;
+    }
+
     // 1. Find the side of the view frustum that the sword is going to swing to
     // 2. Find the point of intersection with that side
     // 3. Swing to it
-    private int FindClosestPlane(Vector3 origin, Vector3 direction)
+    private int FindClosestPlane()
     {
         Vector3 mousePos = Input.mousePosition;
-        mousePos.z = raycastPointDirection.position.z;
+        mousePos.z = raycastPoint.position.z - Mathf.Epsilon;
 
-        Vector3 centerToEdge = (playerCamera.ScreenToWorldPoint(mousePos) - raycastPoint.position).normalized;
-        Ray ray = new Ray(raycastPoint.position, centerToEdge);
-        //Ray ray = new Ray(raycastPoint.position, direction);
-        Debug.DrawRay(raycastPoint.position, centerToEdge * 100f, Color.green, 100f);
+        Vector3 dirToEdge = (playerCamera.ScreenToWorldPoint(mousePos) - raycastPoint.position).normalized;
+        Ray ray = new Ray(raycastPoint.position, dirToEdge);
+
+        //Vector3 dirToEdge = (playerCamera.ScreenToWorldPoint(mousePos) - swordTip.position).normalized;
+        //Ray ray = new Ray(swordTip.position, dirToEdge);
+
+        //Debug.DrawRay(swordTip.position, dirToEdge, Color.green, 10f);
+        //Debug.DrawRay(raycastPoint.position, dirToEdge, Color.green, 10f);
 
         float currentMinDistance = float.MaxValue;
         Plane[] planes = GeometryUtility.CalculateFrustumPlanes(playerCamera);
 
         int nearestSide = -1;
 
-        for (int i = 0; i < 6; ++i)
+        for (int i = 0; i < 4; ++i)
         {
             if (planes[i].Raycast(ray, out float distance))
             {
-                // Since a plane is mathematicaly infinite
-                // what you would want is the one that hits with the shortest ray distance
                 if (distance < currentMinDistance)
                 {
                     currentMinDistance = distance;
                     nearestSide = i;
+                }
+            }
+        }
+
+        if (previousSide == nearestSide)
+        {
+            //dirToEdge = (playerCamera.ScreenToWorldPoint(mousePos) - (swordTip.position + swingDirection * 0.1f)).normalized;
+            //ray = new Ray(swordTip.position + swingDirection * 0.1f, dirToEdge);
+
+            nearestSide = -1;
+            currentMinDistance = float.MaxValue;
+
+            for (int i = 0; i < 4; ++i)
+            {
+                if (i == previousSide)
+                    continue;
+
+                if (planes[i].Raycast(ray, out float distance))
+                {
+                    if (distance < currentMinDistance)
+                    {
+                        currentMinDistance = distance;
+                        nearestSide = i;
+                    }
                 }
             }
         }
@@ -179,44 +231,31 @@ public class CoolSwordAttack : MonoBehaviour
                 Debug.Log("Error");
                 break;
             case 0:
-                Debug.Log("Left");
+                Debug.Log("Left 0");
                 break;
             case 1:
-                Debug.Log("Right");
+                Debug.Log("Right 1");
                 break;
             case 2:
-                Debug.Log("Down");
+                Debug.Log("Down 2");
                 break;
             case 3:
-                Debug.Log("Up");
+                Debug.Log("Up 3");
                 break;
             case 4:
-                Debug.Log("Near");
-                nearestSide = Random.Range(0, 3);
-                break;
-            case 5:
-                Debug.Log("Far");
+                Debug.Log("Near 4");
+                do
+                {
+                    nearestSide = Random.Range(0, 3);
+                } while (nearestSide != previousSide);
                 break;
         }
 
         if (nearestSide == -1)
             return 0;
 
+        previousSide = nearestSide;
+
         return nearestSide;
-    }
-
-    private Vector3 FindPointOfIntersectionWithScreenBorder(Vector3 origin, Vector3 direction)
-    {
-        Ray ray = new Ray(origin, direction);
-        Plane[] planes = GeometryUtility.CalculateFrustumPlanes(playerCamera);
-        Vector3 hitPoint = Vector3.zero;
-
-        // Raycast against the plane
-        if (planes[FindClosestPlane(origin, direction)].Raycast(ray, out float distance))
-        {
-            hitPoint = ray.GetPoint(distance);
-        }
-
-        return hitPoint;
     }
 }
