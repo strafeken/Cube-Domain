15919b67d9b9af3f71f4ca37f5f0eb87fcc16b73
Strafeken <strafeken@gmail.com>
2022-01-11 09:40:41 +0800

    Deleted scene organization folder

--- Assets/Scripts/Enemies/Slime/Slime.cs
+++ Assets/Scripts/Enemies/Slime/Slime.cs
@@ -20,8 +20,6 @@ public class Slime : Enemy
     private Rigidbody rb;
     private Heart heart;
 
-
-
     [Header("Animation")]
     [SerializeField] private Animator animator;
 
@@ -43,24 +41,25 @@ public class Slime : Enemy
 
     [SerializeField] private Vector2 normalJumpForce;
     [SerializeField] private Vector2 attackJumpForce;
+    private Vector3 jumpPosition;
+    private Vector3 resultantJumpForce;
 
     [Header("VFX")]
     [SerializeField] private GameObject attackVFX;
     private GameObject instantiatedVFX;
 
-    private Vector3 jumpPosition;
+    [Header("Others")]
+    private Vector3 centerOfArena;
 
-    //private Material material;
+    private Material material;
     private Color defaultColor;
-
-    private Vector3 resultantJumpForce;
 
     void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
         rb = GetComponent<Rigidbody>();
         heart = GetComponentInChildren<Heart>();
-        //material = GetComponent<Renderer>().material;
+        material = transform.Find("Body").GetComponent<Renderer>().material;
     }
 
     void OnEnable()
@@ -73,10 +72,11 @@ public class Slime : Enemy
     {
         base.Start();
 
-        //defaultColor = material.GetColor("_FresnelColor");
+        centerOfArena = GameObject.FindGameObjectWithTag("CenterOfArena").GetComponent<Transform>().position;
+
+        defaultColor = material.GetColor("_FresnelColor");
 
         SetState(State.SPAWNED);
-        //SetState(State.IDLE);
     }
 
     protected override void Update()
@@ -85,7 +85,7 @@ public class Slime : Enemy
         {
             case State.SPAWNED:
                 {
-                    agent.SetDestination(player.position);
+                    agent.SetDestination(centerOfArena);
 
                     spawnedTimer += Time.deltaTime;
                     if (spawnedTimer > spawnBufferDuration)
@@ -205,22 +205,29 @@ public class Slime : Enemy
                 ResetAnimationToIdle();
                 break;
             case State.IDLE:
+                material.SetColor("_FresnelColor", defaultColor);
+                
                 ResetAnimationToIdle();
-                //material.SetColor("_FresnelColor", defaultColor);
                 break;
             case State.CHASE:
+                material.SetColor("_FresnelColor", defaultColor);
+
                 if(instantiatedVFX)
                     Destroy(instantiatedVFX);
+                
                 attackCooldownTimer = 0f;
-                //material.SetColor("_FresnelColor", defaultColor);
+                
                 ResetAnimationToIdle();
+               
                 jumpCooldownTimer = 0f;
                 break;
             case State.JUMPING:
                 break;
             case State.READY_TO_ATTACK:
-                //material.SetColor("_FresnelColor", new Color(1, 0, 0, 0.5f));
+                material.SetColor("_FresnelColor", new Color(1, 0, 0, 0.5f));
+                
                 ResetAnimationToIdle();
+                
                 instantiatedVFX = Instantiate(attackVFX, new Vector3(transform.position.x, 0.2f, transform.position.z), Quaternion.identity);
                 break;
             case State.DEAD:
