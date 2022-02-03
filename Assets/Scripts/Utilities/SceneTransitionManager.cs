f6b19d0ffa0a004b763366d5f7e64d77e4d4b6f7
Strafeken <strafeken@gmail.com>
2022-02-03 22:21:14 +0800

    revamped ui buttons

--- Assets/Scripts/Utilities/SceneTransitionManager.cs
+++ Assets/Scripts/Utilities/SceneTransitionManager.cs
@@ -14,4 +14,9 @@ public class SceneTransitionManager : MonoBehaviour
     {
         SceneManager.LoadScene("Arena");
     }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 }
